715ca616f16a4a317831ec8d1253d6fbbfc35963
user <user@DESKTOP-0FLM0C6>
2024-07-05 11:51:21 +0900

    first up

---
+++ MediChat/src/main/java/kr/spring/MediChatApplication.java
@@ -0,0 +1,13 @@
+package kr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediChatApplication.class, args);
+	}
+
+}
