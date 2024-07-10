20006afbf0ac472af6fcba3d392cfc5cbc80497a
user <user@DESKTOP-Q0QOFQL>
2024-07-10 16:28:50 +0900

    20240710

--- MediChat/src/main/java/kr/spring/member/controller/MyPageController.java
+++ MediChat/src/main/java/kr/spring/member/controller/MyPageController.java
@@ -1,14 +1,9 @@
 package kr.spring.member.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import kr.spring.doctor.service.DoctorService;
+import kr.spring.doctor.vo.DoctorVO;
 import kr.spring.member.service.MemberService;
-import kr.spring.member.vo.DoctorVO;
 import kr.spring.member.vo.MemberVO;
-import kr.spring.util.AuthCheckException;
-import kr.spring.util.CaptchaUtil;
 import kr.spring.util.FileUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,7 +27,9 @@ public class MyPageController {
 	@Autowired
 	private MemberService memberService;
 	
-
+	@Autowired
+	private DoctorService doctorService;
+	
 	/*==============================
 	 * MY페이지
 	 *==============================*/	
@@ -50,6 +46,7 @@ public class MyPageController {
 		  
 		return "myPage";
 	}
+
 
 
 	/*==============================
@@ -119,11 +116,14 @@ public class MyPageController {
 			 // 비대면 진료 신청 폼 호출
 		    @GetMapping("/member/registerTreat")
 		    public String form(Model model, HttpSession session) {
-		        MemberVO user = (MemberVO) session.getAttribute("user");
+		        DoctorVO user = (DoctorVO) session.getAttribute("user");
 		        DoctorVO doctorVO = new DoctorVO();
 		        if (user != null) {
 		            doctorVO.setMem_name(user.getMem_name());
-		            doctorVO.setDoc_email(user.getMem_email());
+		            doctorVO.setDoc_email(user.getDoc_email());
+		        }
+		        if(user == null) {
+		        	return "redirect:/main/main";
 		        }
 		        model.addAttribute("doctorVO", doctorVO);
 		        return "registerTreat";
@@ -136,23 +136,32 @@ public class MyPageController {
 			    if (result.hasErrors()) {
 			        return form(model,session);
 			    }
+			    DoctorVO doctor = (DoctorVO)session.getAttribute("doctor");
+			    doctorVO.setDoc_num(doctor.getDoc_num());
 
 			    // 현재 로그인한 사용자의 정보 가져오기
-			    MemberVO user = (MemberVO) session.getAttribute("user");
+			    DoctorVO db_doctor = doctorService.selectDoctor(
+	                     doctorVO.getDoc_num());
+				/* DoctorVO loggedInDoctor = doctorService.selectDoctor(user.getMem_num()); */
 			    
-			    DoctorVO loggedInDoctor = doctorService.selectDoctor(user.getMem_num());
-			    
-
-			    // 입력한 비밀번호와 현재 로그인한 사용자의 비밀번호 비교
-			    if (!loggedInDoctor.getDoc_passwd().equals(doctorVO.getDoc_passwd())) {
-			        result.rejectValue("doc_passwd", "password.notMatch", "비밀번호가 일치하지 않습니다.");
-			        return form(model,session);
-			    }
+			  //폼에서 전송한 현재 비밀번호와 DB에서 읽어온 비밀번호 일치 여부 체크	
+				if(!db_doctor.getDoc_passwd().equals(
+						           doctorVO.getNow_passwd())) {
+					result.rejectValue("now_passwd", "invalidPassword");
+					return "myPage";
+				}
+				/*
+				 * // 입력한 비밀번호와 현재 로그인한 사용자의 비밀번호 비교 if
+				 * (!loggedInDoctor.getDoc_passwd().equals(doctorVO.getDoc_passwd())) {
+				 * result.rejectValue("doc_passwd", "password.notMatch", "비밀번호가 일치하지 않습니다.");
+				 * return form(model,session); }
+				 */
 
 			    // 비밀번호가 일치하면 처리 로직 추가
 
 			    return "redirect:/member/docView"; // 성공적으로 처리된 경우 다른 페이지로 리다이렉트
 			}
+			
 }
 
 
