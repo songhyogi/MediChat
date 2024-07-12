fd9742231240e2e715ee45061ab343257824a83b
kyj <user@DESKTOP-B8PE4UO>
2024-07-12 09:03:27 +0900

    채팅방 불러오기

--- MediChat/src/main/java/kr/spring/chat/controller/ChatController.java
+++ MediChat/src/main/java/kr/spring/chat/controller/ChatController.java
@@ -1,5 +1,9 @@
 package kr.spring.chat.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,8 +18,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.spring.chat.service.ChatService;
+import kr.spring.chat.vo.ChatMsgVO;
 import kr.spring.chat.vo.ChatVO;
 import kr.spring.member.vo.MemberVO;
 import kr.spring.reservation.vo.ReservationVO;
@@ -31,8 +37,6 @@ public class ChatController {
 	public ChatVO initCommand() {
 		return new ChatVO();
 	}
-	
-	
 	
 	//비대면채팅 페이지 호출
 	@GetMapping("/chat/chatView")
@@ -61,6 +65,41 @@ public class ChatController {
 		
 		
 		return "chatRoom";
+	}
+	
+	//채팅창 들어가면 채팅 불러오기
+	@GetMapping("/chat/chatRoom")
+	@ResponseBody
+	public Map<String, Object> getMessage(HttpSession session,
+										  @RequestParam("chat_num") long chat_num){
+		log.debug("채팅 입장, 번호: "+ chat_num);
+		
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		List<ChatMsgVO> msg_list = new ArrayList<ChatMsgVO>();
+		ReservationVO reservation = chatService.selectReservationByChatNum(chat_num);
+		
+		//파라미터를 날짜 및 시각으로 파싱
+		LocalDate resDate = LocalDate.parse(reservation.getRes_date(), DateTimeFormatter.ISO_DATE);
+		LocalTime resTime = LocalTime.parse(reservation.getRes_time(), DateTimeFormatter.ofPattern("HH:mm"));
+		
+		//local 시각과 비교할 날짜와 시각으로 파싱
+		LocalDateTime reservationDateTime = LocalDateTime.of(resDate, resTime);
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		
+		if(LocalDateTime.now().isAfter(reservationDateTime)){
+			log.debug("채팅방 사용 가능- 예약 날짜/시간: "+resDate+"/"+resTime);
+			msg_list = chatService.selectMsg(chat_num);
+			map.put("list",msg_list);
+			map.put("chat","open");
+		}else{
+			log.debug("채팅방 사용 불가- 예약 날짜/시간: "+resDate+"/"+resTime);
+			map.put("chat","close");
+		}
+		
+	 return map;
+		
 	}
 	
 	//예약 확정 버튼 클릭 시 채팅방 생성
--- MediChat/src/main/java/kr/spring/chat/service/ChatServiceImpl.java
+++ MediChat/src/main/java/kr/spring/chat/service/ChatServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kr.spring.chat.dao.ChatMapper;
 import kr.spring.chat.vo.ChatFileVO;
+import kr.spring.chat.vo.ChatMsgVO;
 import kr.spring.chat.vo.ChatPaymentVO;
 import kr.spring.chat.vo.ChatVO;
 import kr.spring.reservation.vo.ReservationVO;
@@ -28,17 +29,16 @@ public class ChatServiceImpl implements ChatService{
 	public List<ChatVO> selectChatListForDoc(long mem_num) {
 		return chatMapper.selectChatListForDoc(mem_num);
 	}
+	
+	@Override
+	public ReservationVO selectReservationByChatNum(long chat_num) {
+		return chatMapper.selectReservationByChatNum(chat_num);
+	}
 
 	@Override
 	public void createChat(ReservationVO reservationVO) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public ChatVO selectChatDetail(long res_num) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -57,6 +57,12 @@ public class ChatServiceImpl implements ChatService{
 	public void updateChatStatus(long chat_num) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public List<ChatMsgVO> selectMsg(long chat_num) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
