6a955446974d86028eba22106d8aa73d997037b1
songhyogi <thdhk1230@gmail.com>
2024-07-14 23:37:00 +0900

    스케줄 관리

--- MediChat/src/main/java/kr/spring/schedule/dao/ScheduleMapper.java
+++ MediChat/src/main/java/kr/spring/schedule/dao/ScheduleMapper.java
@@ -6,29 +6,33 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import kr.spring.holiday.vo.HolidayVO;
 
 @Mapper
 public interface ScheduleMapper {
-	//정기휴무요일 가져오기
-	@Select("SELECT doc_off FROM doctor_detail WHERE doc_num=#{doc_num}")
-	public String getRegularDayoff(Long doc_num);
-	
-	//근무시작시간, 근무종료시간 가져오기
-	@Select("SELECT doc_stime,doc_etime FROM doctor_detail WHERE doc_num=#{doc_num}")
-	public Map<String,String> getWorkingHours(Long doc_num);
-	
-	//개별휴무일 가져오기
-	@Select("SELECT * FROM holiday WHERE doc_num=#{doc_num}")
-	public List<HolidayVO> getHoliday(Long doc_num);
-	
-	//개별휴무일 등록
-	@Insert("INSERT INTO holiday(holi_num,doc_num,holi_date,holi_time,holi_status) VALUES(holi_seq.nextval,#{doc_num},#{holi_date},#{holi_time},#{holi_status})")
-	public void insertHoliday(Long doc_num);
-	
-	//휴무시간 가져오기
-	@Select("SELECT doff_time FROM dayoff WHERE doc_num=#{doc_num} AND doff_date=#{doff_date}")
-	public List<String> getDayoffTimes(Long doc_num, String doff_date);
-	
+    // 정기휴무요일 가져오기
+    @Select("SELECT doc_off FROM doctor_detail WHERE doc_num=#{doc_num}")
+    public String getRegularDayoff(Long doc_num);
+
+    // 근무시작시간, 근무종료시간 가져오기
+    @Select("SELECT doc_stime, doc_etime FROM doctor_detail WHERE doc_num=#{doc_num}")
+    public Map<String, String> getWorkingHours(Long doc_num);
+
+    // 개별휴무일 가져오기
+    @Select("SELECT * FROM holiday WHERE doc_num=#{doc_num}")
+    public List<HolidayVO> getHoliday(Long doc_num);
+
+    // 개별휴무일 등록
+    @Insert("INSERT INTO holiday (holi_num, doc_num, holi_date, holi_time, holi_status) VALUES (holi_seq.nextval, #{doc_num}, #{holi_date}, #{holi_time}, #{holi_status})")
+    public void insertHoliday(HolidayVO holiday);
+
+    // 개별휴무일 업데이트
+    @Update("UPDATE holiday SET holi_status = #{holi_status} WHERE doc_num = #{doc_num} AND holi_date = #{holi_date} AND holi_time = #{holi_time}")
+    public void updateHoliday(HolidayVO holiday);
+
+    // 개별휴무일 존재 여부 확인
+    @Select("SELECT COUNT(*) FROM holiday WHERE doc_num = #{doc_num} AND holi_date = #{holi_date} AND holi_time = #{holi_time}")
+    public int countHoliday(HolidayVO holiday);
 }
--- MediChat/src/main/java/kr/spring/schedule/service/ScheduleService.java
+++ MediChat/src/main/java/kr/spring/schedule/service/ScheduleService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import kr.spring.holiday.vo.HolidayVO;
 import kr.spring.schedule.vo.DayoffVO;
@@ -17,6 +18,10 @@ public interface ScheduleService {
 	public Map<String,String> getWorkingHours(Long doc_num);
 	//개별휴무일 가져오기
 	public List<HolidayVO> getHoliday(Long doc_num);
-	//개별휴무 등록
-	public void insertHoliday(Long doc_num);
+	//개별휴무일 등록
+    public void insertHoliday(HolidayVO holiday);
+    //개별휴무일 업데이트
+    public void updateHoliday(HolidayVO holiday);
+    //개별휴무일 존재 여부 확인
+    public int countHoliday(HolidayVO holiday);
 }
