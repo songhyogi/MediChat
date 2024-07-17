b274479bef0310d48bd84b8137ac33dcafe3c497
KimSeungYeon <tmddus3002@gmail.com>
2024-07-17 17:39:39 +0900

    commit

--- MediChat/src/main/java/kr/spring/notification/vo/NotificationVO.java
+++ MediChat/src/main/java/kr/spring/notification/vo/NotificationVO.java
@@ -1,5 +1,6 @@
 package kr.spring.notification.vo;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 public class NotificationVO {
 	private Long noti_num;				//알림 식별자
 	private Long mem_num;				//받는 회원 식별자
--- MediChat/src/main/webapp/WEB-INF/views/template/header.jsp
+++ MediChat/src/main/webapp/WEB-INF/views/template/header.jsp
@@ -160,4 +160,5 @@
 		} else {
 			headerRegisterDiv.style.display = 'block';
 		}
+	};
 </script>
